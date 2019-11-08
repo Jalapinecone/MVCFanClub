3ea99947100c73c41e843a2a924fd4b5aaba20c1
rayotte <55161408+rayotte@users.noreply.github.com>
2019-11-08 15:38:00 -0500

    Save function using (reference to) vectors

--- COMP3004 D1/saveProgram.h
+++ COMP3004 D1/saveProgram.h
@@ -1,11 +1,17 @@
 #ifndef SAVEPROGRAM_H
 #define SAVEPROGRAM_H
 #include <string>
+#include <vector>
 #include "functionInterface.h"
 
 class saveProgram : public functionInterface
 {
 	public:
-		void execute();
+		void execute(vector<string> &v);
+	private:
+		string text;
+		string fileName;
+		std::vector<string> lines;
+
 };
 #endif
